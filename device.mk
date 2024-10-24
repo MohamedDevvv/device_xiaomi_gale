72f58f8f554a16c1dcbb57542940fc8027d5e561
MohamedDevvv <aaamohamed499@gmail.com>
2024-10-24 13:40:32 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -333,7 +333,7 @@ PRODUCT_PACKAGES += \
     WifiResOverlayGale
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-blaze
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
